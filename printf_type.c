71bcc17fef8956f23604fb3c8d63a4fc37fdeecb
THO-Gaetan <9771@holbertonstudents.com>
2024-11-26 12:03:24 +0100

    new files to stock function

--- printf_type.c
+++ printf_type.c
@@ -1,25 +1,5 @@
 #include "main.h"
 
-int printf_write(char format, va_list args)
-{
-	int n = 0;
-
-	if (format == 'c')
-		n += printf_char(format, args);
-	else if (format == 's')
-		n += printf_str(format, args);
-	else if (format == 'd' || format == 'i')
-		n += printf_int(format, args);
-	else if (format == '%')
-		n += _putchar('%');
-	else
-	{
-		n += _putchar('%');
-		n += _putchar(format);
-	}
-	return (n);
-}
-
 int printf_char(char format, va_list args)
 {
 	int n = 0;
