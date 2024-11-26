78287146934139115610c32dfd3485fc53eea415
THO-Gaetan <9771@holbertonstudents.com>
2024-11-26 11:12:32 +0100

    Fixing if str is null, starting to ad printf_int

--- main.h
+++ main.h
@@ -5,9 +5,10 @@
 #include <unistd.h>
 
 int _putchar (char c);
-
+int printf_char(char format, va_list args);
+int printf_str(char format, va_list args);
+int printf_int(char format, va_list args);
 int printf_write(char format, va_list args);
-
 int _printf(const char *format, ...);
 
 #endif
--- printf_write.c
+++ printf_write.c
@@ -3,23 +3,13 @@
 int printf_write(char format, va_list args)
 {
 	int n = 0;
-	char *str;
 
 	if (format == 'c')
-	{
-		n += _putchar(va_arg(args, int));
-	}
+		n += printf_char(format, args);
 	else if (format == 's')
-	{
-		str = va_arg(args, char*);
-		if (!str)
-			str = "(nil)";
-		while (*str)
-		{
-			n += _putchar(*str);
-			str++;
-		}
-	}
+		n += printf_str(format, args);
+	else if (format == 'd')
+		n += printf_int(format, args);
 	else if (format == '%')
 		n += _putchar('%');
 	else
@@ -27,5 +17,44 @@ int printf_write(char format, va_list args)
 		n += _putchar('%');
 		n += _putchar(format);
 	}
+	return (n);
+}
+
+int printf_char(char format, va_list args)
+{
+	int n = 0;
+
+	if (format == 'c')
+		n += _putchar(va_arg(args, int));
+	return (n);
+}
+
+int printf_str(char format, va_list args)
+{
+	int n = 0;
+	char *str;
+
+	if (format == 's')
+	{
+	str = va_arg(args, char*);
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		n += _putchar(*str);
+		str++;
+	}
+	}
+	return (n);
+}
+
+int printf_int(char format, va_list args)
+{
+	int n = 0;
+	int i;
+
+	i = va_arg(args, int);
+	if (format == 'd')
+		n += write(1, &i, 4);
 	return (n);
 }
